06bcbe4820f2d200dfa4d146765acd807412ab99
MK <mertkoksal88@gmail.com>
2020-07-23 13:23:34 +0300

    completed setting up firebase intergration

---
+++ My Offices App/SignInVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Offices App
+//
+//  Created by Mert Köksal on 23.07.2020.
+//  Copyright © 2020 Mert Köksal. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,17 @@
+
+platform :ios, '9.0'
+
+target 'My Offices App' do
+  
+  use_frameworks!
+  
+  pod ‘Firebase/Database’
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Storage'
+  pod 'FBSDKLoginKit'
+  pod 'SwiftKeychainWrapper'
+
+  # Pods for My Offices App
+
+end
